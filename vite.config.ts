ae609ea361bcd38dad9f8edce33e29c664a1e532
Jin_Y <wlsdud6221@naver.com>
2025-06-28 01:54:23 +0900

    style : prettier 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
-import path from "node:path";
-import { crx } from "@crxjs/vite-plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import zip from "vite-plugin-zip-pack";
-import tailwindcss from "@tailwindcss/vite";
+import path from 'node:path'
+import { crx } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import zip from 'vite-plugin-zip-pack'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-import manifest from "./manifest.config.js";
-import { name, version } from "./package.json";
+import manifest from './manifest.config.js'
+import { name, version } from './package.json'
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": `${path.resolve(__dirname, "src")}`,
+      '@': `${path.resolve(__dirname, 'src')}`,
     },
   },
   plugins: [
     react(),
     crx({ manifest }),
     tailwindcss(),
-    tsconfigPaths()
-    zip({ outDir: "release", outFileName: `crx-${name}-${version}.zip` }),
+    tsconfigPaths(),
+    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
   ],
   server: {
     cors: {
       origin: [/chrome-extension:\/\//],
     },
   },
-});
+})
