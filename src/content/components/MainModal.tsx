324cd09e38a694bb3998a4992c004f85edfeeab7
JinY <wlsdud6221@naver.com>
2025-07-04 04:22:12 +0900

    design : 메인모달과 버튼 디자인 수정 #5

--- src/content/components/MainModal.tsx
+++ src/content/components/MainModal.tsx
@@ -1,3 +1,42 @@
+import { motion } from 'framer-motion'
+import type { Variants } from 'framer-motion'
+
+const modalVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { type: 'spring', stiffness: 100, damping: 12, mass: 0.5 },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.8,
+    transition: { type: 'spring', stiffness: 150, damping: 18, mass: 0.5 },
+  },
+}
+
 export const MainModal = () => {
-  return <div></div>
+  return (
+    <motion.div
+      variants={modalVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      transition={{ duration: 0.3 }}
+      className="bg-kongju fixed z-500 h-[600px] w-[350px] origin-bottom-right overflow-hidden rounded-[36px]"
+      style={{ bottom: '96px', right: '25px' }}
+    >
+      <div className="p-4">
+        <button>토큰 생성 테스트</button>
+        <div></div>
+        <button>토큰 생성 테스트</button> <div></div>
+        <button>토큰 생성 테스트</button> <div></div>
+        <button>토큰 생성 테스트</button> <div></div>
+        <button>토큰 생성 테스트</button>
+      </div>
+    </motion.div>
+  )
 }
--- src/content/components/ModalButton.tsx
+++ src/content/components/ModalButton.tsx
@@ -7,7 +7,7 @@ const CloseOverlay = () => (
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ duration: 0.2 }}
-    className="bg-opacity-30 absolute inset-0 bg-black backdrop-blur-sm"
+    className="bg-opacity-10 absolute inset-0 rounded-2xl bg-black/20 backdrop-blur-sm"
   >
     <div className="absolute inset-0 flex items-center justify-center text-white">
       <X size={24} />
@@ -19,8 +19,12 @@ export const ModalButton = ({ isOpen, onClick }: { isOpen: boolean; onClick: () 
   return (
     <div
       onClick={onClick}
-      className="fixed right-2 bottom-2 z-500 h-[56px] w-[56px] bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${chrome.runtime.getURL('/assets/popup_getToken.png')})` }}
+      className="fixed z-500 h-[56px] w-[56px] cursor-pointer rounded-2xl bg-cover bg-center bg-no-repeat"
+      style={{
+        bottom: '25px',
+        right: '25px',
+        backgroundImage: `url(${chrome.runtime.getURL('/assets/popup_getToken.png')})`,
+      }}
     >
       <AnimatePresence> {isOpen && <CloseOverlay />}</AnimatePresence>
     </div>
