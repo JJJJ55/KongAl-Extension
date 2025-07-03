f2623f29dfd9bd89e16b46910b5701b05671ce64
Jin_Y <wlsdud6221@naver.com>
2025-07-03 17:55:06 +0900

    design : content modal 디자인 분할 #5

--- src/content/components/ModalButton.tsx
+++ src/content/components/ModalButton.tsx
@@ -1,9 +1,28 @@
-export const ModalButton = () => {
+import { AnimatePresence, motion } from 'framer-motion'
+import { X } from 'lucide-react'
+
+const CloseOverlay = () => (
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.2 }}
+    className="bg-opacity-30 absolute inset-0 bg-black backdrop-blur-sm"
+  >
+    <div className="absolute inset-0 flex items-center justify-center text-white">
+      <X size={24} />
+    </div>
+  </motion.div>
+)
+
+export const ModalButton = ({ isOpen, onClick }: { isOpen: boolean; onClick: () => void }) => {
   return (
-    // fixed right-0 bottom-0 z-100 m-5 h-[48px] w-[48px] bg-cover bg-center bg-no-repeat
     <div
+      onClick={onClick}
       className="fixed right-2 bottom-2 z-500 h-[56px] w-[56px] bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${chrome.runtime.getURL('/assets/popup_getToken.png')})` }}
-    ></div>
+    >
+      <AnimatePresence> {isOpen && <CloseOverlay />}</AnimatePresence>
+    </div>
   )
 }
