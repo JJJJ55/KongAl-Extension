06f3e330de7b6934508b2422a86557ddbd9c966a
Jin_Y <wlsdud6221@naver.com>
2025-07-06 01:48:26 +0900

    design : 세부 과목 모달 띄우기 #6

--- src/content/components/subject/SubjectCard.tsx
+++ src/content/components/subject/SubjectCard.tsx
@@ -4,11 +4,15 @@ const ReportIssue = () => <span className="bg-kongju rounded-2xl px-2 text-[10px
 const ClassIssue = () => <span className="bg-chenan rounded-2xl px-2 text-[10px] text-white">강의 확인</span>
 const BoardIssue = () => <span className="bg-yesan rounded-2xl px-2 text-[10px] text-white">공지 확인</span>
 
-export const SubjectCard = () => {
+type SubjectProps = {
+  onClick: () => void
+}
+export const SubjectCard = ({ onClick }: SubjectProps) => {
   return (
     <div
-      className="bg-knuBlue flex h-[60px] w-[282px] justify-end rounded-xl"
+      className="bg-knuBlue flex h-[60px] w-[282px] cursor-pointer justify-end rounded-xl"
       style={{ boxShadow: '0 5px 10px rgba(0,0,0,0.3)' }}
+      onClick={onClick}
     >
       <div className="flex h-full w-[277px] flex-col justify-center rounded-xl bg-white p-5">
         <div>
--- src/content/components/subject/SubjectPage.tsx
+++ src/content/components/subject/SubjectPage.tsx
@@ -1,24 +1,28 @@
+import { useState } from 'react'
 import { LoadingSkeleton } from './LoadingSkeleton'
 import { NotFound } from './NotFound'
 import { SubjectCard } from './SubjectCard'
 import { TopNavBar } from './TopNavbar'
+import { SubjectDetailPage } from './detail/SubjectDetailPage'
 
-export const SubjectPage = () => {
+export const SubjectPage = ({ onClick }: { onClick: () => void }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <TopNavBar />
       <div className="scrollbar-hidden flex flex-1 flex-col items-center gap-6 overflow-auto py-5">
-        {/* <SubjectCard />
-        <SubjectCard />
-        <SubjectCard />
-        <SubjectCard />
-        <SubjectCard />
-        <SubjectCard />
-        <SubjectCard />
-        <SubjectCard />
-        <SubjectCard /> */}
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
+        <SubjectCard onClick={onClick} />
         {/* <LoadingSkeleton /> */}
-        <NotFound />
+        {/* <NotFound /> */}
       </div>
     </>
   )
