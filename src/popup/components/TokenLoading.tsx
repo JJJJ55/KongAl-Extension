23edf6ccf93f663c264c91224c7e711a1a4a57e2
Jin_Y <wlsdud6221@naver.com>
2025-07-09 17:07:42 +0900

    design : 팝업 디자인 요소 수정 close #9

--- src/popup/components/TokenLoading.tsx
+++ src/popup/components/TokenLoading.tsx
@@ -5,7 +5,7 @@ import loading from 'public/Loading.json'
 export const TokenLoading = () => {
   return (
     <main className="mt-4 flex flex-col items-center">
-      <Text className="mb-2 text-[20px] font-bold">"공주대 LMS 알리미 - 콩알"</Text>
+      <Text className="mb-2 text-[20px] font-bold">공주대 LMS 알리미 - 콩알</Text>
       <Text className="mt-4 text-[15px] font-bold">서비스 토큰을 생성 중입니다.</Text>
       <Text className="text-center text-[12px] whitespace-pre">잠시만 기다려 주세요.</Text>
       <Lottie animationData={loading} loop={true} />
