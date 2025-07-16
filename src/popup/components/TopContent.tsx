83a5f3cf78c4ea88ec483bc42c581ff3fe8b0d01
JinY <wlsdud6221@naver.com>
2025-07-17 03:12:45 +0900

    fix : 상태별 팝업 변동 #16

--- src/popup/components/TopContent.tsx
+++ src/popup/components/TopContent.tsx
@@ -1,43 +1,64 @@
 import { SmallButton, Text } from '@/components'
-import { useStoragestore } from '@/store/useStorageStore'
-import type { StorageData } from '@/types'
+import { Guide } from '@/constants'
 import { useState } from 'react'
 
 type TopContentProps = {
+  noti: boolean
   token: string | null
   updateFn: (token: string | null) => void
 }
 
-export const TopContent = ({ token, updateFn }: TopContentProps) => {
+export const TopContent = ({ noti, token, updateFn }: TopContentProps) => {
   const [value, setValue] = useState<string | null>(null)
 
-  const getInfo = () => {
-    chrome.runtime.sendMessage({ type: 'USER_INFO' }, response => {
-      console.log(response)
-    })
+  const PopUpMain = () => {
+    return (
+      <>
+        {noti ? (
+          <>
+            <img src={Guide.notification.img} alt="캐릭터" />
+            <Text className="mt-4 text-[15px] font-bold">{Guide.notification.bigText}</Text>
+            <Text className="text-center text-[12px] whitespace-pre">{Guide.notification.smallText}</Text>
+          </>
+        ) : (
+          <>
+            <img src={token ? Guide.getToken.img : Guide.noToken.img} alt="캐릭터" />
+            <Text className="mt-4 text-[15px] font-bold">{token ? Guide.getToken.bigText : Guide.noToken.bigText}</Text>
+            <Text className="text-center text-[12px] whitespace-pre">
+              {token ? Guide.getToken.smallText : Guide.noToken.smallText}
+            </Text>
+          </>
+        )}
+      </>
+    )
   }
 
   return (
     <main className="mt-4 flex flex-col items-center">
       <Text className="mb-2 text-[20px] font-bold">공주대 LMS 알리미 - 콩알</Text>
-      <img src="/assets/popup_notToken.png" alt="캐릭터" onClick={getInfo} />
-      <Text className="mt-4 text-[15px] font-bold">서비스 이용을 위해 토큰 생성을 해주세요!</Text>
-      <Text className="text-center text-[12px] whitespace-pre">
-        {`계졍 변경 등 토큰 삭제를 원할 경우 \n브라우저 오른쪽 하단에서 설정창을 확인해주세요.`}
-      </Text>
+      <PopUpMain />
       {!token && (
-        <div className="mt-2 flex gap-2">
-          <input
-            type="text"
-            className="border-gray4 h-[40px] w-[250px] rounded-xl border px-2 text-[12px] font-bold"
-            placeholder="토큰을 입력해주세요."
-            value={value!}
-            onChange={e => setValue(e.target.value)}
-          />
-          <SmallButton onClick={() => updateFn(value)} className="w-[80px]">
-            등 록
-          </SmallButton>
-        </div>
+        <>
+          <a
+            className="text-14px text-knuBlue cursor-pointer font-bold"
+            href="https://knulms.kongju.ac.kr/profile/settings"
+            target="_blank"
+          >
+            LMS 토큰 생성 바로가기
+          </a>
+          <div className="mt-2 flex gap-2">
+            <input
+              type="text"
+              className="border-gray4 h-[40px] w-[250px] rounded-xl border px-2 text-[12px] font-bold"
+              placeholder="토큰을 입력해주세요."
+              value={value!}
+              onChange={e => setValue(e.target.value)}
+            />
+            <SmallButton onClick={() => updateFn(value)} className="w-[80px]">
+              등 록
+            </SmallButton>
+          </div>
+        </>
       )}
     </main>
   )
