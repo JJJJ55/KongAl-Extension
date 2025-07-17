9abc082c0e9f1dae4020573cc204e18cd9740d38
JinY <wlsdud6221@naver.com>
2025-07-18 04:48:57 +0900

    chore : 과목, 이슈 정의 #20

--- src/background/index.ts
+++ src/background/index.ts
@@ -12,5 +12,34 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       .catch(error => sendResponse({ success: false, data: error.message }))
 
     return true
+  } else if (message.type === 'USER_SUBJECT') {
+    fetch(`${import.meta.env.VITE_SITETOKEN_URL}users/self/favorites/courses?include[]=teachers`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Authorization: `Bearer ${message.token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(res => sendResponse({ success: true, data: res }))
+      .catch(error => sendResponse({ success: false, data: error.message }))
+
+    return true
+  } else if (message.type === 'USER_ISSUE') {
+    fetch(
+      `${import.meta.env.VITE_SITETOKEN_URL}/planner/items?context_codes[]=course_40378&start_date=2025-07-10T15:00:00.000Z&per_page=100`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          Authorization: `Bearer ${message.token}`,
+        },
+      },
+    )
+      .then(response => response.json())
+      .then(res => sendResponse({ success: true, data: res }))
+      .catch(error => sendResponse({ success: false, data: error.message }))
+
+    return true
   }
 })
