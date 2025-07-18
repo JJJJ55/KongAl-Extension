0ff6a1daca8f0831dc92d0fabcdfc70386620eb9
Jin_Y <wlsdud6221@naver.com>
2025-07-18 21:17:24 +0900

    feat : 로딩 스켈레톤 적용 #20

--- src/content/components/subject/SubjectPage.tsx
+++ src/content/components/subject/SubjectPage.tsx
@@ -3,14 +3,14 @@ import { LoadingSkeleton } from './LoadingSkeleton'
 import { NotFound } from './NotFound'
 import { SubjectCard } from './SubjectCard'
 import { TopNavBar } from './TopNavbar'
-import { SubjectDetailPage } from './detail/SubjectDetailPage'
 import { useStoragestore } from '@/store/useStorageStore'
 
 export const SubjectPage = ({ onClick }: { onClick: () => void }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const { settings, contents, updateData } = useStoragestore()
 
   const handleGetSubject = () => {
+    setIsLoading(true)
     chrome.runtime.sendMessage({ type: 'USER_SUBJECT', token: settings.siteToken }, response => {
       if (response.success) {
         const res = response.data.map(data => {
@@ -25,19 +25,25 @@ export const SubjectPage = ({ onClick }: { onClick: () => void }) => {
         console.log(res)
       } else {
         window.alert('올바른 토큰이 아닙니다. 다시 확인해주세요.')
+        //토스트 메세지
       }
     })
+    setIsLoading(false)
   }
 
   return (
     <>
       <TopNavBar onClick={handleGetSubject} />
-      <div className="flex flex-col items-center flex-1 gap-3 py-3 overflow-auto scrollbar-hidden">
-        {contents.courseList.map(data => (
-          <SubjectCard courseId={data.courseId} title={data.title} teacher={data.teacher} onClick={onClick} />
-        ))}
-        {/* <LoadingSkeleton /> */}
-        {/* <NotFound /> */}
+      <div className="scrollbar-hidden flex flex-1 flex-col items-center gap-3 overflow-auto py-3">
+        {isLoading ? (
+          <LoadingSkeleton />
+        ) : contents.courseList.length ? (
+          contents.courseList.map(data => (
+            <SubjectCard courseId={data.courseId} title={data.title} teacher={data.teacher} onClick={onClick} />
+          ))
+        ) : (
+          <NotFound />
+        )}
       </div>
     </>
   )
