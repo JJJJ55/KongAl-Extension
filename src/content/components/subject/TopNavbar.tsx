f793078da063903fd4aedab9433ca8384ec66567
Jin_Y <wlsdud6221@naver.com>
2025-07-21 02:29:48 +0900

    test : 이슈 불러오기 임시 버튼 생성 #20

--- src/content/components/subject/TopNavbar.tsx
+++ src/content/components/subject/TopNavbar.tsx
@@ -1,11 +1,11 @@
 import { LogoIcon, Text } from '@/components'
 
-export const TopNavBar = ({ onClick }: { onClick: () => void }) => {
+export const TopNavBar = ({ onClick, onIssueTest }: { onClick: () => void; onIssueTest: () => void }) => {
   return (
     <div
       onClick={onClick}
       // style={{ boxShadow: '0 0 30px rgba(0,0,0,0.3)' }}
-      className="flex flex-row items-center justify-start p-5 bg-white rounded-2xl"
+      className="flex flex-row items-center justify-start rounded-2xl bg-white p-5"
       style={{ boxShadow: '0 3px 3px rgba(0,0,0,0.2)', height: '60px' }}
     >
       <LogoIcon className="ml-1 h-[36px] w-[36px]" />
@@ -13,6 +13,8 @@ export const TopNavBar = ({ onClick }: { onClick: () => void }) => {
         <Text className="text-[16px] font-bold">콩알</Text>
         <Text className="text-[11px]">공주대 LMS 알림을 한번에!</Text>
       </div>
+      {/* 임시용 이슈 불러오기 삭제예정 */}
+      <button onClick={onIssueTest}>이슈</button>
     </div>
   )
 }
