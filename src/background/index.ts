cbb53c8e008443408f1911c007add5a546a2b1c0
Jin_Y <wlsdud6221@naver.com>
2025-07-21 21:30:36 +0900

    refactor : 이슈체크 수정 #20

--- src/background/index.ts
+++ src/background/index.ts
@@ -26,16 +26,19 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
     return true
   } else if (message.type === 'USER_ISSUE') {
-    fetch(
-      `${import.meta.env.VITE_SITETOKEN_URL}/planner/items?context_codes[]=course_40378&start_date=2025-07-10T15:00:00.000Z&per_page=100`,
-      {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          Authorization: `Bearer ${message.token}`,
-        },
-      },
+    const { token, ids } = message
+
+    const url = new URL(
+      `${import.meta.env.VITE_SITETOKEN_URL}/planner/items?start_date=2025-03-10T15:00:00.000Z&per_page=100`,
     )
+    ids.forEach(id => url.searchParams.append('context_codes[]', `course_${id}`))
+    fetch(url.toString(), {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
       .then(response => response.json())
       .then(res => sendResponse({ success: true, data: res }))
       .catch(error => sendResponse({ success: false, data: error.message }))
