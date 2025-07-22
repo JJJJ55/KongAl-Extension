78c3683da05b0929a6a599db1592e1266bef92e8
Jin_Y <wlsdud6221@naver.com>
2025-07-23 01:52:31 +0900

    feat : 과목 주차 학습 추가 #20

--- src/content/components/subject/detail/DetailPlay.tsx
+++ src/content/components/subject/detail/DetailPlay.tsx
@@ -1,20 +1,67 @@
 import { LineText } from '@/components'
 import { PlayCard } from './PlayCard'
+import { useStoragestore } from '@/store/useStorageStore'
+import { NotFound } from '../NotFound'
+import type { PlayItem } from '@/types'
 
-export const DetailPlay = ({ courseId }: { courseId: string | undefined }) => {
+export const DetailPlay = ({ courseId }: { courseId: string | '' }) => {
+  const { contents, updateData } = useStoragestore()
+
+  const handleGetPlay = (id: string) => {
+    chrome.runtime.sendMessage({ type: 'SUBJECT_LIST', id }, response => {
+      // updateData('contents',prev => ({...prev, 'courseDetail'}))
+      if (response.success) {
+        const newValue: Record<string, PlayItem> = {}
+        const currentDetail = contents.courseDetail[id] || {
+          PlayList: {},
+          BoardList: {},
+          ReportList: {},
+        }
+        for (const data of response.data) {
+          for (const d of data.module_items) {
+            const { title, position, content_type, completed } = d
+            newValue[position] = {
+              title,
+              isComplete: completed,
+              isAttendance: null,
+              dueAt: null,
+            }
+            if (content_type === 'attendance_item') {
+              const { use_attendance, omit_progress } = d.content_data
+              if (use_attendance === true && omit_progress === false) {
+                newValue[position].isAttendance = d.attendance_status
+              }
+            }
+            newValue[position].dueAt = d.content_data.due_at
+          }
+        }
+
+        updateData('contents', prev => {
+          const newCourseDetail = { ...prev.courseDetail }
+          newCourseDetail[id] = {
+            ...currentDetail,
+            PlayList: {
+              ...newValue,
+            },
+          }
+
+          return { ...prev, courseDetail: newCourseDetail }
+        })
+      } else {
+        alert('실패')
+      }
+    })
+  }
+
   return (
     <div className="scrollbar-hidden flex flex-1 flex-col items-center gap-3 overflow-auto py-3">
-      <LineText className="w-[60px] text-[12px] font-bold">1주차</LineText>
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
-      <PlayCard />
+      <button onClick={() => handleGetPlay(courseId)}>임시</button>
+      {/* <LineText className="w-[60px] text-[12px] font-bold">1주차</LineText> */}
+      {Object.keys(contents.courseDetail[courseId].PlayList).length ? (
+        Object.entries(contents.courseDetail[courseId].PlayList).map(([week, p]) => <PlayCard key={week} data={p} />)
+      ) : (
+        <NotFound />
+      )}
     </div>
   )
 }
--- src/content/components/subject/detail/PlayCard.tsx
+++ src/content/components/subject/detail/PlayCard.tsx
@@ -1,24 +1,31 @@
 import { Text, TimeIcon } from '@/components'
+import type { PlayItem } from '@/types'
+import clsx from 'clsx'
 import { ClockAlert } from 'lucide-react'
 
+type PlayProps = {
+  data: PlayItem
+}
 const UnCheckIssue = () => <span className="bg-kongju rounded-2xl px-1 text-[10px] text-white">미 출 석</span>
 const CheckIssue = () => <span className="bg-knuGreen rounded-2xl px-2 text-[10px] text-white">출 석</span>
 
-export const PlayCard = () => {
+export const PlayCard = ({ data }: PlayProps) => {
   return (
     <div
-      className="bg-kongju flex h-[60px] w-[282px] cursor-pointer justify-end rounded-xl"
+      className={clsx(
+        'flex h-[60px] w-[282px] cursor-pointer justify-end rounded-xl',
+        data.isComplete ? 'bg-knuGreen' : 'bg-kongju',
+      )}
       style={{ boxShadow: '0 3px 3px rgba(0,0,0,0.2)' }}
     >
       <div className="flex h-full w-[277px] flex-col justify-center gap-2 rounded-xl bg-white p-5">
         <div className="flex items-center justify-between">
-          <Text className="w-[190px] truncate text-[13px]">과제과제과제과제과제과제과제과제과제</Text>
-          {/* <CheckIssue /> */}
-          <UnCheckIssue />
+          <Text className="w-[190px] truncate text-[13px]">{data.title}</Text>
+          {data.isAttendance !== null ? data.isAttendance ? <CheckIssue /> : <UnCheckIssue /> : ''}
         </div>
         <div className="flex items-center">
           <TimeIcon className="h-[16px] w-[16px]" />
-          <Text className="ml-1 text-[12px] font-bold">마감 2일 전</Text>
+          <Text className="ml-1 text-[12px] font-bold">`마감 ${data.dueAt} 전`</Text>
         </div>
       </div>
     </div>
