8ad5583ea355ce646851e206372a1be8ae72fad1
Jin_Y <wlsdud6221@naver.com>
2025-07-23 22:15:44 +0900

    test : 주차학습 임시요소 #20

--- src/content/components/subject/detail/DetailTopNav.tsx
+++ src/content/components/subject/detail/DetailTopNav.tsx
@@ -5,16 +5,17 @@ type DetailTopNavProps = {
   title: string | undefined
   teacher: string | undefined
   onClick: () => void
+  onGet: () => void
 }
 
-export const DetailTopNav = ({ title, teacher, onClick }: DetailTopNavProps) => {
+export const DetailTopNav = ({ title, teacher, onClick, onGet }: DetailTopNavProps) => {
   return (
     <header
       className="flex flex-row items-center rounded-2xl bg-white p-5"
       style={{ boxShadow: '0 3px 3px rgba(0,0,0,0.2)', height: '60px' }}
     >
       <InfoIcon className="h-[20px] w-[20px] flex-1/12" />
-      <div className="ml-5 flex-10/12">
+      <div className="ml-5 flex-10/12" onClick={onGet}>
         <Text className="w-[230px] truncate text-[15px] font-bold">{title}</Text>
         <Text className="text-[11px]">{`${teacher} 교수`}</Text>
       </div>
