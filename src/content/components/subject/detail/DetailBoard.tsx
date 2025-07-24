c18572fee01702b07b87c1e12acbae239db0ab9d
Jin_Y <wlsdud6221@naver.com>
2025-07-24 20:38:35 +0900

    refactor : 리스트 내림차순 정렬 #26

--- src/content/components/subject/detail/DetailBoard.tsx
+++ src/content/components/subject/detail/DetailBoard.tsx
@@ -4,10 +4,13 @@ import { NotFound } from '../NotFound'
 
 export const DetailBoard = ({ courseId }: { courseId: string | '' }) => {
   const { contents } = useStoragestore()
+
   return (
     <div className="scrollbar-hidden flex flex-1 flex-col items-center gap-3 overflow-auto py-3">
       {Object.keys(contents.courseDetail[courseId].BoardList).length ? (
-        Object.values(contents.courseDetail[courseId].BoardList).map(b => <BoardCard key={b.createAt} data={b} />)
+        Object.values(contents.courseDetail[courseId].BoardList)
+          .sort((a, b) => new Date(b.createAt).getTime() - new Date(a.createAt).getTime())
+          .map(b => <BoardCard key={b.html_url} data={b} />)
       ) : (
         <NotFound />
       )}
