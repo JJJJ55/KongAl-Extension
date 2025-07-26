c8148f4224a6ffa3772427532020ed1e7bca80c8
Jin_Y <wlsdud6221@naver.com>
2025-07-26 21:25:09 +0900

    chore : 데이터 패치 주기 추가 #26

--- src/store/useStorageStore.ts
+++ src/store/useStorageStore.ts
@@ -22,7 +22,7 @@ const initialStorageData: StorageData = {
     xToken: null,
     version: pkg.version,
     updateAt: '2025-01-01T00:00:00.000Z',
-    fetchCycle: null,
+    fetchCycle: 1000 * 60 * 60 * 12,
     image: chrome.runtime.getURL('/kongal_Logo.png'),
   },
   contents: {
