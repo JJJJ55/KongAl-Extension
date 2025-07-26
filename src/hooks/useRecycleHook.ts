6292c68063b7788018fbbf3db90fc1a6c345ca3c
Jin_Y <wlsdud6221@naver.com>
2025-07-26 21:25:29 +0900

    feat : 데이터 주기 계산 훅 #26

---
+++ src/hooks/useRecycleHook.ts
@@ -0,0 +1,22 @@
+import { useStoragestore } from '@/store/useStorageStore'
+import { useEffect, useState } from 'react'
+
+export const useRefreshCheck = () => {
+  const { settings } = useStoragestore()
+  const [shouldRefresh, setShouldRefresh] = useState(false)
+
+  useEffect(() => {
+    const lastUpdateTime = new Date(settings.updateAt).getTime()
+    const currentTime = new Date().getTime()
+    const refreshTime = settings.fetchCycle
+
+    console.log('마지막 업뎃', lastUpdateTime)
+    console.log('현재', currentTime)
+    console.log('업데이트 텀', refreshTime)
+    console.log('결과', currentTime - lastUpdateTime)
+
+    if (refreshTime !== null) setShouldRefresh(currentTime - lastUpdateTime > refreshTime)
+  }, [settings.updateAt, settings.fetchCycle])
+
+  return { shouldRefresh }
+}
