da68439c2cb51b28dd3f59fda43cbce7200702cc
Jin_Y <wlsdud6221@naver.com>
2025-07-29 00:58:45 +0900

    refactor : 데이터 함수 간결화 #28

--- src/content/components/subject/detail/DetailBoard.tsx
+++ src/content/components/subject/detail/DetailBoard.tsx
@@ -1,40 +1,14 @@
 import { useStoragestore } from '@/store/useStorageStore'
 import { BoardCard } from './BoardCard'
 import { NotFound } from '../NotFound'
+import { ChangeBoardStatus } from '@/utils/ChangeStatus'
 
 export const DetailBoard = ({ courseId }: { courseId: string | '' }) => {
   const { contents, updateData } = useStoragestore()
 
   const handleLink = (link: string, bid: string, isOk: boolean) => {
     if (!isOk) {
-      updateData('contents', prev => {
-        const newCourseDetail = { ...prev.courseDetail }
-        const mergedCourseList = { ...prev.courseList }
-
-        const currentDetail = newCourseDetail[courseId]
-        const boardInfo = currentDetail.BoardList[bid]
-
-        newCourseDetail[courseId] = {
-          ...newCourseDetail[courseId],
-          BoardList: {
-            ...currentDetail.BoardList,
-            [bid]: {
-              ...boardInfo,
-              isOk: true,
-            },
-          },
-        }
-        mergedCourseList[courseId] = {
-          ...mergedCourseList[courseId],
-          isBoard: mergedCourseList[courseId].isBoard - 1,
-        }
-
-        return {
-          ...prev,
-          courseDetail: newCourseDetail,
-          courseList: mergedCourseList,
-        }
-      })
+      ChangeBoardStatus({ courseId, itemId: bid, updateFn: updateData })
     }
     window.location.href = `${import.meta.env.VITE_REDIRECT_URL}${link}`
   }
@@ -43,7 +17,9 @@ export const DetailBoard = ({ courseId }: { courseId: string | '' }) => {
       {Object.keys(contents.courseDetail[courseId].BoardList).length ? (
         Object.entries(contents.courseDetail[courseId].BoardList)
           .sort(([, a], [, b]) => new Date(b.createAt).getTime() - new Date(a.createAt).getTime())
-          .map(([bid, b]) => <BoardCard key={b.html_url} data={b} onLink={() => handleLink(b.html_url, bid, b.isOk)} />)
+          .map(([bid, b], idx) => (
+            <BoardCard key={b.html_url} index={idx} data={b} onLink={() => handleLink(b.html_url, bid, b.isOk)} />
+          ))
       ) : (
         <NotFound />
       )}
