b85736c4674fcf0197592d2097c38d7863c926d9
Jin_Y <wlsdud6221@naver.com>
2025-07-29 00:59:54 +0900

    feat : 데이터 상태 변경 모듈 추가 #28

---
+++ src/utils/ChangeStatus.ts
@@ -0,0 +1,94 @@
+import type { StorageData } from '@/types'
+import { ChangeDutAt } from './FormatDate'
+
+type ChangeStatusProps = {
+  courseId: string
+  itemId: string
+  isFlag?: boolean
+  updateFn: <K extends keyof StorageData>(key: K, update: (prev: StorageData[K]) => StorageData[K]) => Promise<void>
+}
+
+export const ChangeBoardStatus = ({ courseId, itemId, updateFn }: ChangeStatusProps) => {
+  updateFn('contents', prev => {
+    const newCourseDetail = { ...prev.courseDetail }
+    const mergedCourseList = { ...prev.courseList }
+
+    const currentDetail = newCourseDetail[courseId]
+    const boardInfo = currentDetail.BoardList[itemId]
+
+    newCourseDetail[courseId] = {
+      ...newCourseDetail[courseId],
+      BoardList: {
+        ...currentDetail.BoardList,
+        [itemId]: {
+          ...boardInfo,
+          isOk: true,
+        },
+      },
+    }
+    mergedCourseList[courseId] = {
+      ...mergedCourseList[courseId],
+      isBoard: mergedCourseList[courseId].isBoard - 1,
+    }
+
+    return {
+      ...prev,
+      courseDetail: newCourseDetail,
+      courseList: mergedCourseList,
+    }
+  })
+}
+
+export const ChangeReportStatus = ({ courseId, itemId, isFlag, updateFn }: ChangeStatusProps) => {
+  updateFn('contents', prev => {
+    const newCourseDetail = { ...prev.courseDetail }
+    const mergedCourseList = { ...prev.courseList }
+
+    const currentDetail = newCourseDetail[courseId]
+    const ReportInfo = currentDetail.ReportList[itemId]
+
+    if (isFlag) {
+      newCourseDetail[courseId] = {
+        ...newCourseDetail[courseId],
+        ReportList: {
+          ...currentDetail.ReportList,
+          [itemId]: {
+            ...ReportInfo,
+            isOk: false,
+            isChange: false,
+          },
+        },
+      }
+      if (ChangeDutAt(ReportInfo.dueAt) !== '마감') {
+        mergedCourseList[courseId] = {
+          ...mergedCourseList[courseId],
+          isReport: mergedCourseList[courseId].isReport + 1,
+        }
+      }
+    } else {
+      newCourseDetail[courseId] = {
+        ...newCourseDetail[courseId],
+        ReportList: {
+          ...currentDetail.ReportList,
+          [itemId]: {
+            ...ReportInfo,
+            isOk: true,
+            isChange: true,
+          },
+        },
+      }
+      if (ChangeDutAt(ReportInfo.dueAt) !== '마감') {
+        mergedCourseList[courseId] = {
+          ...mergedCourseList[courseId],
+          isReport: mergedCourseList[courseId].isReport - 1,
+        }
+      }
+    }
+
+    return {
+      ...prev,
+      courseDetail: newCourseDetail,
+      courseList: mergedCourseList,
+    }
+  })
+}
