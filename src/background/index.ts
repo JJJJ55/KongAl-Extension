c71b420521d83be730203ff05def081f73a324cb
Jin_Y <wlsdud6221@naver.com>
2025-07-30 22:16:12 +0900

    feat : 알림 띄우기 #30

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,5 @@
+import type { NotificationItem } from '@/types'
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'USER_INFO') {
     fetch(import.meta.env.VITE_USER_INFO, {
@@ -35,15 +37,44 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
     return true
   } else if (message.type === 'NOTI') {
-    // 알림 테스트
-    chrome.notifications.create({
-      type: 'basic',
-      iconUrl: chrome.runtime.getURL('kongal_Logo.png'), // public 폴더에 icon.png 넣어두세요
-      title: 'API 응답 도착',
-      message: `알림 발생`,
-      priority: 2,
+    console.log('받은 알림 목록', message.notification)
+    message.notification.forEach(({ title, message }: NotificationItem) => {
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: chrome.runtime.getURL('kongal_Logo.png'), // public 폴더에 icon.png 넣어두세요
+        title,
+        message,
+        priority: 2,
+      })
     })
-    // return true
+
+    // 알림 테스트
+    // const items = [
+    //   { title: '새로운 공지', message: '' },
+    //   { title: '공지 2', message: '탄소시장과에너지저장장치 01분반' },
+    //   { title: '공지 3', message: '시험 일정 업데이트1' },
+    //   { title: '공지 4', message: '시험 일정 업데이트2' },
+    //   { title: '학습 1', message: '새로운 학습1' },
+    //   { title: '학습 2', message: '새로운 학습2' },
+    //   { title: '학습 3', message: '새로운 학습3' },
+    //   { title: '학습 4', message: '새로운 학습4' },
+    //   { title: '과제 1', message: '오늘 마감 과제1' },
+    //   { title: '과제 2', message: '오늘 마감 과제2' },
+    // ]
+
+    // for (let i = 0; i < items.length; i += 4) {
+    //   const item = items.slice(i, i + 4)
+
+    //   chrome.notifications.create({
+    //     type: 'list',
+    //     iconUrl: chrome.runtime.getURL('kongal_Logo.png'), // public 폴더에 icon.png 넣어두세요
+    //     title: `공주대 LMS 알리미 - 콩알`,
+    //     message: `${message.content}`,
+    //     contextMessage: '아아',
+    //     priority: 2,
+    //     items: item,
+    //   })
+    // }
   }
 })
 
