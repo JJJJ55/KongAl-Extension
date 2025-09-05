f9fb7067bd3a735baf68d405f233663f66f4b4ed
Jin_Y <wlsdud6221@naver.com>
2025-09-05 23:50:06 +0900

    refactor : 팝업 로딩 버벅임 해결 #48

--- src/popup/components/PopupNav.tsx
+++ src/popup/components/PopupNav.tsx
@@ -4,13 +4,13 @@ import { NavLink } from './NavLink'
 export const PopupNav = () => {
   return (
     <nav className="border-knuBlue flex w-full justify-center gap-6">
-      <NavLink link="https://github.com/JJJJ55" text="Github">
+      <NavLink link="https://github.com/JJJJ55/KongAl-Extension" text="Github">
         <GithubIcon className="h-[20px] w-[20px]" />
       </NavLink>
-      <NavLink link="https://github.com/JJJJ55" text="문의하기">
+      <NavLink link="https://open.kakao.com/o/s9tYshQh" text="문의 & 오류접수">
         <CallIcon className="h-[20px] w-[20px]" />
       </NavLink>
-      <NavLink link="https://github.com/JJJJ55" text="개발자 후원">
+      <NavLink link="https://buymeacoffee.com/dhwlsdud62z" text="개발자 후원">
         <BuymecoffeeIcon className="h-[20px] w-[20px]" />
       </NavLink>
     </nav>
