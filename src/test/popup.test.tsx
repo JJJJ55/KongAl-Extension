bf75dfd0b6417942150253f6960f65d96333534c
Jin_Y <wlsdud6221@naver.com>
2025-09-07 22:14:06 +0900

    comment : 주석 제거 #55

--- src/test/popup.test.tsx
+++ src/test/popup.test.tsx
@@ -42,7 +42,6 @@ describe('Popup', () => {
 
     render(<App />)
 
-    // async await로 대기
     expect(await screen.findByText('LMS 토큰 생성 바로가기')).toBeInTheDocument()
   })
 })
@@ -62,42 +61,3 @@ describe('ContentScript', () => {
     expect(screen.getByText('공주대 LMS 알림을 한번에!')).toBeInTheDocument()
   })
 })
-
-// describe('서비스 로그인', () => {
-//   beforeEach(() => {
-//     vi.clearAllMocks()
-//   })
-
-//   it('팝업에서 로그인 후 contentScript가 열리는지 확인', async () => {
-//     // chrome.tabs.query.mockImplementation((_q: any, cb: any) => {
-//     //   setTimeout(() => cb([{ id: 1, url: 'https://knulms.kongju.ac.kr/' }]), 0)
-//     //   return Promise.resolve([{ id: 1 }])
-//     // })
-
-//     // chrome.tabs.sendMessage.mockImplementation((_id: number, msg: any, cb: any) => {
-//     //   setTimeout(() => {
-//     //     if (msg?.type === 'GET_LMS') {
-//     //       cb({ success: true, lmsUser: '홍길동', xToken: 'token-123' })
-//     //     } else {
-//     //       cb({ success: true })
-//     //     }
-//     //   }, 0)
-//     //   return Promise.resolve({ success: true })
-//     // })
-
-//     // 팝업 + 컨텐츠를 같은 트리에 붙여서 테스트
-//     render(
-//       <>
-//         <App />
-//         <Content />
-//       </>,
-//     )
-
-//     // 팝업 버튼 클릭
-//     const button = await screen.findByTestId('modal-button')
-//     fireEvent.click(button)
-
-//     // contentScript 안의 제목 확인
-//     expect(await screen.findByText('공주대 LMS 알림을 한번에!')).toBeInTheDocument()
-//   })
-// })
