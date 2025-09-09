5e8e1a3b88ee20273d242f9072984ba995555ab3
Jin_Y <wlsdud6221@naver.com>
2025-09-09 23:37:49 +0900

    design : 하단 UI 중복 코드 수정 #63

--- src/content/components/subject/BottomNavbar.tsx
+++ src/content/components/subject/BottomNavbar.tsx
@@ -1,5 +1,5 @@
-import { SettingIcon, SubjectIcon, Text } from '@/components'
-import clsx from 'clsx'
+import { BottomNavContent, SettingIcon, SubjectIcon } from '@/components'
+import { BottomNavItem } from './BottomNavItem'
 
 type BottomNavProps = {
   activeType: 'subjects' | 'settings'
@@ -8,30 +8,13 @@ type BottomNavProps = {
 
 export const BottomNavBar = ({ activeType, setActiveType }: BottomNavProps) => {
   return (
-    <div
-      className="dark:bg-dark12 flex justify-around rounded-2xl bg-white transition-colors duration-500"
-      style={{ boxShadow: '0 -5px 5px -5px rgba(0,0,0,0.3)', height: '70px' }}
-    >
-      <button
-        className={clsx(
-          'flex flex-1 cursor-pointer flex-col items-center justify-center px-[16px] py-[12px] text-[12px]',
-          activeType === 'subjects' ? 'text-knuBlue dark:text-positive' : 'text-gray4',
-        )}
-        onClick={() => setActiveType('subjects')}
-      >
+    <BottomNavContent>
+      <BottomNavItem activeType={activeType} setActiveType={setActiveType} item="subjects" itemName="과 목">
         <SubjectIcon />
-        <Text>과 목</Text>
-      </button>
-      <button
-        className={clsx(
-          'flex flex-1 cursor-pointer flex-col items-center justify-center px-[16px] py-[12px] text-[12px]',
-          activeType === 'settings' ? 'text-knuBlue dark:text-positive' : 'text-gray4',
-        )}
-        onClick={() => setActiveType('settings')}
-      >
+      </BottomNavItem>
+      <BottomNavItem activeType={activeType} setActiveType={setActiveType} item="settings" itemName="설 정">
         <SettingIcon />
-        <Text>설 정</Text>
-      </button>
-    </div>
+      </BottomNavItem>
+    </BottomNavContent>
   )
 }
