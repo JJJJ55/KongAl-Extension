cc5a493f8a643f5719a05a9e9984c888ecff8f28
Jin_Y <wlsdud6221@naver.com>
2025-09-11 02:35:02 +0900

    refactor : 테마 훅 정의로 코드 분리 #67

--- src/content/components/MainModal.tsx
+++ src/content/components/MainModal.tsx
@@ -3,9 +3,10 @@ import type { Variants } from 'framer-motion'
 import { SubjectPage } from './subject/SubjectPage'
 import { BottomNavBar } from './subject/BottomNavbar'
 import { SettingPage } from './setting/SettingPage'
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useStoragestore } from '@/store/useStorageStore'
 import ToastComponent from '@/components/ToastComponent'
+import { useThemeCheck } from '@/hooks/useThemeHook'
 
 const modalVariants: Variants = {
   hidden: {
@@ -26,24 +27,12 @@ const modalVariants: Variants = {
 
 export const MainModal = ({ isLoading }: { isLoading: boolean }) => {
   const [activeType, setActiveType] = useState<'subjects' | 'settings'>('subjects')
+  const { mainRef } = useThemeCheck()
   const { system } = useStoragestore()
 
   const ActiveContent = useMemo(() => {
     return activeType === 'subjects' ? <SubjectPage isLoading={isLoading} /> : <SettingPage />
   }, [activeType, isLoading])
-
-  const mainRef = useRef<HTMLDivElement>(null)
-  useEffect(() => {
-    if (!mainRef.current) return
-
-    const root = mainRef.current
-    const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-    if (system.theme === 'dark' || (system.theme === 'sys' && systemDark)) {
-      root.classList.add('dark')
-    } else {
-      root.classList.remove('dark')
-    }
-  }, [system.theme])
 
   return (
     <motion.div
@@ -53,7 +42,13 @@ export const MainModal = ({ isLoading }: { isLoading: boolean }) => {
       exit="exit"
       transition={{ duration: 0.3 }}
       className="bg-bgColor fixed z-500 h-[600px] w-[350px] origin-bottom-right overflow-hidden rounded-3xl shadow-[0_0_100px_0_rgba(0,0,0,0.2)] backdrop-blur-sm"
-      style={{ boxShadow: ' 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)', bottom: '96px', right: '25px' }}
+      style={{
+        boxShadow: ' 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+        ...(system.pos === 'tl' && { top: '96px', left: '25px' }),
+        ...(system.pos === 'tr' && { top: '96px', right: '25px' }),
+        ...(system.pos === 'bl' && { bottom: '96px', left: '25px' }),
+        ...(system.pos === 'br' && { bottom: '96px', right: '25px' }),
+      }}
     >
       <div ref={mainRef} className="dark:bg-dark flex h-full flex-col transition-colors duration-500">
         {ActiveContent}
---
+++ src/hooks/useThemeHook.ts
@@ -0,0 +1,21 @@
+import { useStoragestore } from '@/store/useStorageStore'
+import { useEffect, useRef } from 'react'
+
+export const useThemeCheck = () => {
+  const mainRef = useRef<HTMLDivElement>(null)
+  const { system } = useStoragestore()
+
+  useEffect(() => {
+    if (!mainRef.current) return
+
+    const root = mainRef.current
+    const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    if (system.theme === 'dark' || (system.theme === 'sys' && systemDark)) {
+      root.classList.add('dark')
+    } else {
+      root.classList.remove('dark')
+    }
+  }, [system.theme])
+
+  return { mainRef }
+}
